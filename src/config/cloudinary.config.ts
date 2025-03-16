36e7315cf2ccaf8f6c79e873df4d54103b79cb4e
Chocopain <101320467+VinhThanh438@users.noreply.github.com>
2025-03-16 23:30:48 +0700

    Update cloudinary.config.ts

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -2,9 +2,9 @@ import cloudinary from 'cloudinary';
 const cloudinaryVersion = cloudinary.v2;
 
 cloudinaryVersion.config({
-    cloud_name: 'djz9u9dcc',
-    api_key: '218827861586773',
-    api_secret: 'ucSerBzLbKWGjeA6OncABTjVG4Y'
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
 });
 
 export const cloudinaryConfig = cloudinaryVersion;
